0b4e14ce5730b0c21743df5b0e5210c4c9c7048f
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-21 12:38:46 -0500

    Create photo widget display

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,12 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def home
+    if params[:user_id]
+      @user_id = params[:user_id]
+      flickr = Flickr.new(ENV['FLICKR_API_KEY'], ENV['FLICKR_SHARED_SECRET'])
+      
+      @photos = flickr.people.getPublicPhotos(user_id: @user_id)
+    end
+  end
 end
